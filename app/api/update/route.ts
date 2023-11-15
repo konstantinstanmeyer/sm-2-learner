3ff504911aedf0170917e66a4fed0891f644c0b3
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-11-15 17:52:52 -0500

    update card basic funcitonality (update later)

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -1,12 +1,24 @@
 import mongoDBConnection from "@/lib/mongodb/connection";
 import User from "@/models/user";
 import { NextRequest, NextResponse } from "next/server";
+import { Card } from "@/types/models"
+import sm2 from "@/lib/sm2"
 
 export async function POST(request: NextRequest) {
-  const { email, EF, repitition, interval, cardID } = await request.json();
-  console.log("hello there")
-  await mongoDBConnection();
-  console.log("2222")
-  await User.create({ email });
-  return NextResponse.json({ message: "User Registered" }, { status: 201 });
+    const { email, userGrade, cardID } = await request.json();
+    const filter = { email: email };
+
+    await mongoDBConnection();
+
+    const user = await User.findOne(filter);
+    const cardIndex = user.cards.findIndex((c: Card) => c._id === cardID);
+    const card = user.cards[cardIndex];
+
+    const newCard = sm2({card, userGrade});
+
+    user.cards[cardIndex] = newCard;
+
+    user.save();
+
+    return NextResponse.json({ message: "User Card Updated" }, { status: 200 });
 }
