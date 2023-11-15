b982ceca8ee58440907c2a22d2c112d9d2a984ce
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-11-14 22:40:28 -0500

    basic card component functionality

--- components/card.tsx
+++ components/card.tsx
@@ -1,7 +1,26 @@
-export default function Card(){
-    return (
-        <div className="card">
+"use client"
 
+import { useState } from "react";
+
+interface sample {
+    original: string;
+    translation: string;
+}
+
+const sample1: sample = {
+    original: "La ensalada lleva berro y espinaca",
+    translation: "The salad has watercress and spinach"
+}
+
+export default function Card(){
+    const [isClicked, setIsClicked] = useState<boolean>(false);
+
+    return (
+        <div onClick={() => setIsClicked(!isClicked)} className="card card-1">
+            {isClicked ? 
+            <p>{sample1.original}</p>
+            :
+            <p>{sample1.translation}</p>}
         </div>
     )
 }
