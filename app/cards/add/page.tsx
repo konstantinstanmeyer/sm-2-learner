a96a5a66a41b19632d401f588d04ae816fdafa14
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-11-29 15:14:03 -0500

    functional image convertion to base64, pre-comp.

--- app/cards/add/page.tsx
+++ app/cards/add/page.tsx
@@ -1,11 +1,33 @@
+"use client"
 
+const WIDTH = 144;
+
+import convertToBase64 from "@/util/convertToBase64";
+import { ChangeEvent, useState } from "react";
 
 export default function AddCards(){
+    const [image, setImage] = useState<string>("")
+    
+    async function handleFileUpload(e: ChangeEvent){
+        const target= e.target as HTMLInputElement;
+        const file: File = (target.files as FileList)[0];
 
+        const image = (await convertToBase64(file)) as string;
+        setImage(image);
+    }
     
     return (
-        <div>
-
+        <div className="mt-[10vh]">
+            <form>
+                <input 
+                    type="file"
+                    name="myFile"
+                    id='file-upload'
+                    accept='.jpeg, .png, .jpg'
+                    onChange={(e: ChangeEvent) => handleFileUpload(e)}
+                />
+            </form>
+            <img src={image} />
         </div>
     )
 }
--- components/CardOptions.tsx
+++ components/CardOptions.tsx
@@ -1,7 +1,10 @@
 export default function CardOptions(){
     return (
-        <div className="flex absolute flex-row items-center [&>button]:bg-gray-300 [&>button]:mx-1 [&>button]:px-2 [&>button]:rounded-sm [&>button]:py-1 bottom-2">
-            <button>1 <span className="text-xs">(no clue)</span></button>
+        <div className="flex absolute flex-row h-fit items-center [&>button]:justify-center [&>button]:min-h-[3rem] [&>button]:flex [&>button]:flex-col [&>button]:items-center [&>button]:bg-gray-300 [&>button]:mx-1 [&>button]:px-2 [&>button]:rounded-sm [&>button]:py-1 bottom-2">
+            <button className="">
+                <p>1</p>
+                <p className="text-xs inline-block">(no clue)</p>
+            </button>
             <button>2</button>
             <button>3 <span className="text-xs">(familiar)</span></button>
             <button>4</button>
