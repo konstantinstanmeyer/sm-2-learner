738021c030626af9fc5e9bfd3078e798b59029f1
KonstantinStanmeyer <konstantin@stanmeyer.com>
2024-09-29 16:36:33 -0400

    export fix page.tsx/addcard

--- app/cards/add/page.tsx
+++ app/cards/add/page.tsx
@@ -113,7 +113,7 @@ export default function AddCards(){
     )
 }
 
-export async function dataUrlToFile(dataUrl: string, fileName: string): Promise<File> {
+async function dataUrlToFile(dataUrl: string, fileName: string): Promise<File> {
     const res: Response = await fetch(dataUrl);
     const blob: Blob = await res.blob();
     return new File([blob], fileName, { type: 'image/png' });
